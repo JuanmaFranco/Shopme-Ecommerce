a2e5e0540c6580c0a3c55517915893cb4922fcc0
JuanmaFranco <juanmartin_franco@hotmail.com>
2023-08-03 20:14:17 -0300

    feat: Add UserService, UserController, and users.html page
    Created the UserService to handle business logic related to users, and the UserController to manage user-related HTTP requests
    Also, added the users.html page to display the list of users in a table format

---
+++ ShopmeProject/ShopmeWebParent/ShopmeBackend/src/main/java/com/shopme/admin/user/UserController.java
@@ -0,0 +1,27 @@
+package com.shopme.admin.user;
+
+import com.shopme.common.entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
+public class UserController {
+
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/users")
+    public String listAllUsers(Model model) {
+        List<User> users = userService.listAll();
+        model.addAttribute("users", users);
+        return "users";
+    }
+}
