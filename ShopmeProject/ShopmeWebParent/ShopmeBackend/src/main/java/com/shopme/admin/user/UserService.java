cd26f22caf9d82e655c78ece735a5e69dae4d1b5
JuanmaFranco <juanmartin_franco@hotmail.com>
2023-08-04 13:59:03 -0300

    feat: Add Spring Security and BCrypt password encryption
    Implemented Spring Security to secure the application and added functionality to encrypt user passwords using BCrypt before storing them in the database. Tested the feature by adding a new user through the form, and verified that the password is properly encrypted and persisted in the database

--- ShopmeProject/ShopmeWebParent/ShopmeBackend/src/main/java/com/shopme/admin/user/UserService.java
+++ ShopmeProject/ShopmeWebParent/ShopmeBackend/src/main/java/com/shopme/admin/user/UserService.java
@@ -3,6 +3,7 @@ package com.shopme.admin.user;
 import com.shopme.common.entity.Role;
 import com.shopme.common.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -12,11 +13,13 @@ public class UserService {
 
     private final UserRepository userRepository;
     private final RoleRepository roleRepository;
+    private final PasswordEncoder passwordEncoder;
 
     @Autowired
-    public UserService(UserRepository userRepository, RoleRepository roleRepository) {
+    public UserService(UserRepository userRepository, RoleRepository roleRepository, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
         this.roleRepository = roleRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     public List<User> listAll() {
@@ -28,6 +31,12 @@ public class UserService {
     }
 
     public void save(User user) {
+        encodePassword(user);
         userRepository.save(user);
+    }
+
+    public void encodePassword(User user) {
+        String encodedPassword = passwordEncoder.encode(user.getPassword());
+        user.setPassword(encodedPassword);
     }
 }
