b72e4dc9261a5ef6aa6975e795232c2f6d9b4672
JuanmaFranco <juanmartin_franco@hotmail.com>
2023-08-16 19:16:55 -0300

    feat: Add logout functionality for user session
    Implemented the ability for users to log out of their sessions.
    Added a logout button and configured the necessary components to handle the logout process.
    This ensures that users can securely terminate their sessions and log out of the application.
    Tested and verified the logout functionality to ensure its proper functioning.

--- ShopmeProject/ShopmeWebParent/ShopmeBackend/src/main/java/com/shopme/admin/security/WebSecurityConfig.java
+++ ShopmeProject/ShopmeWebParent/ShopmeBackend/src/main/java/com/shopme/admin/security/WebSecurityConfig.java
@@ -3,18 +3,14 @@ package com.shopme.admin.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-
-import javax.sql.DataSource;
 
 @Configuration
 public class WebSecurityConfig implements WebSecurityCustomizer {
@@ -52,6 +48,8 @@ public class WebSecurityConfig implements WebSecurityCustomizer {
                 .loginPage("/login")
                 .usernameParameter("email")
                 .permitAll());
+
+        http.logout(LogoutConfigurer::permitAll);
 
         return http.build();
     }
