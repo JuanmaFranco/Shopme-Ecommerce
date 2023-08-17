9ea66fab33d87b31102f1e2b5a9e0dee46596cc5
JuanmaFranco <juanmartin_franco@hotmail.com>
2023-08-17 11:53:30 -0300

    feat: Add remember me functionality
    Implemented the "remember me" feature by adding a checkbox labeled "Remember me" during login.
    The generated remember-me token has a validity of 604,800 seconds (one week), allowing users to stay logged in even after restarting the application.

--- ShopmeProject/ShopmeWebParent/ShopmeBackend/src/main/java/com/shopme/admin/security/WebSecurityConfig.java
+++ ShopmeProject/ShopmeWebParent/ShopmeBackend/src/main/java/com/shopme/admin/security/WebSecurityConfig.java
@@ -3,17 +3,20 @@ package com.shopme.admin.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.AbstractRequestMatcherRegistry;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
+import org.springframework.security.config.annotation.web.configurers.RememberMeConfigurer;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-public class WebSecurityConfig implements WebSecurityCustomizer {
+public class WebSecurityConfig {
 
     @Bean
     public PasswordEncoder passwordEncoder() {
@@ -37,6 +40,17 @@ public class WebSecurityConfig implements WebSecurityCustomizer {
     @Bean
     protected SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
+        http.authorizeHttpRequests(customizer -> customizer
+                .requestMatchers(
+                        "/images/**",
+                        "/js/**",
+                        "/webjars/**",
+                        "/style.css",
+                        "/fontawesome/**"
+                )
+                .permitAll()
+        );
+
         http.authorizeHttpRequests(configurer -> configurer
                 .anyRequest()
                 .authenticated()
@@ -47,21 +61,17 @@ public class WebSecurityConfig implements WebSecurityCustomizer {
         http.formLogin(configurer -> configurer
                 .loginPage("/login")
                 .usernameParameter("email")
-                .permitAll());
+                .permitAll()
+        );
 
         http.logout(LogoutConfigurer::permitAll);
+
+        http.rememberMe(configurer -> configurer
+                .key("AbcDefgHijKlmnOpqrs_1234567890")
+                .tokenValiditySeconds(60 * 60 * 24 * 7)  // one week
+                .userDetailsService(userDetailsService()));
 
         return http.build();
     }
 
-    @Override
-    public void customize(WebSecurity web) {
-        web.ignoring().requestMatchers(
-                "/images/**",
-                "/js/**",
-                "/webjars/**",
-                "/style.css",
-                "/fontawesome/**"
-        );
-    }
 }
