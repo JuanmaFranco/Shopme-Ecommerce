4da890f34b0110e0faff55152ad2c11499b64644
JuanmaFranco <juanmartin_franco@hotmail.com>
2023-08-18 20:42:55 -0300

    test: Add tests for category and subcategory creation
    Implement test methods to ensure proper creation and persistence of categories and subcategories using the CategoryRepository.
    The following tests were added:
    
        - testCreateRootCategory()
        - testCreateSubcategory()
        - testGetCategory()
        - testPrintHierarchicalCategories()
    
    Additionally, a utility method printChildren(parent, sublevel) was introduced to print subcategories of a given category, aiding in visualizing the hierarchical structure.
    
    All tests are passing and confirm the correct functionality of category and subcategory management.

--- ShopmeProject/ShopmeWebParent/ShopmeBackend/src/main/java/com/shopme/admin/category/CategoryRepository.java
+++ ShopmeProject/ShopmeWebParent/ShopmeBackend/src/main/java/com/shopme/admin/category/CategoryRepository.java
@@ -1,10 +1,10 @@
 package com.shopme.admin.category;
 
 import com.shopme.common.entity.Category;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
-public interface CategoryRepository extends PagingAndSortingRepository<Category, Integer> {
-
+public interface CategoryRepository extends PagingAndSortingRepository<Category, Integer>, CrudRepository<Category, Integer> {
 
 
 }
---
+++ ShopmeProject/ShopmeWebParent/ShopmeBackend/src/test/java/com/shopme/admin/category/CategoryRepositoryTests.java
@@ -0,0 +1,100 @@
+package com.shopme.admin.category;
+
+import com.shopme.common.entity.Category;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.annotation.Rollback;
+
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.*;
+
+@DataJpaTest(showSql = false)
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@Rollback(value = false)
+public class CategoryRepositoryTests {
+
+    private final CategoryRepository categoryRepository;
+
+    @Autowired
+    public CategoryRepositoryTests(CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
+    }
+
+    @Test
+    public void testCreateRootCategory() {
+        Category category = new Category("Electronics");
+        Category savedCategory = categoryRepository.save(category);
+
+        assertThat(savedCategory.getId()).isGreaterThan(0);
+    }
+
+    @Test
+    public void testCreateSubcategory() {
+        Category parent = new Category(7);
+        Category subcategory = new Category("iPhone", parent);
+
+        Category savedCategory = categoryRepository.save(subcategory);
+
+        assertThat(savedCategory.getId()).isGreaterThan(0);
+    }
+
+    @Test
+    public void testGetCategory() {
+
+        Category category = categoryRepository.findById(2).get();
+
+        System.out.println(category.getName());
+
+        Set<Category> children = category.getChildren();
+
+        for (Category child : children) {
+            System.out.println(child.getName());
+        }
+
+        System.out.println();
+
+        children.forEach(cat -> System.out.println(cat.getName()));
+
+        assertThat(children.size()).isGreaterThan(0);
+    }
+
+    @Test
+    public void testPrintHierarchicalCategories() {
+        Iterable<Category> categories = categoryRepository.findAll();
+
+        for (Category category : categories) {
+            if (category.getParent() == null) {
+                System.out.println(category.getName());
+
+                Set<Category> children = category.getChildren();
+
+                for (Category subcategory : children) {
+                    System.out.println("--" + subcategory.getName());
+                    printChildren(subcategory, 1);
+                }
+            }
+
+            System.out.println();
+        }
+    }
+
+    private void printChildren(Category parent, int subLevel) {
+        int newSubLevel = subLevel + 1;
+        Set<Category> children = parent.getChildren();
+        for (Category subcategory : children) {
+
+            for (int i = 0; i < newSubLevel; i++) {
+                System.out.print("--");
+            }
+
+            System.out.println(subcategory.getName());
+
+            printChildren(subcategory, newSubLevel);
+        }
+    }
+
+
+}
