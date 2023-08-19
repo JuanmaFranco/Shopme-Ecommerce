b4a5783002372474b6b883cac4fddcd681ab36cb
JuanmaFranco <juanmartin_franco@hotmail.com>
2023-08-19 09:52:18 -0300

    feat: Add Category service and controller
    Implemented the CategoryService class to handle business logic related to categories.
    Created the corresponding CategoryController to manage interactions with the front end.
    
    Tested the functionality by displaying stored categories from the database in the categories.html form, confirming successful retrieval and display of categories.

---
+++ ShopmeProject/ShopmeWebParent/ShopmeBackend/src/main/java/com/shopme/admin/category/CategoryService.java
@@ -0,0 +1,24 @@
+package com.shopme.admin.category;
+
+import com.shopme.common.entity.Category;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CategoryService {
+
+    private final CategoryRepository categoryRepository;
+
+    @Autowired
+    public CategoryService(CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
+    }
+
+    public List<Category> listAll() {
+        return (List<Category>) categoryRepository.findAll();
+    }
+
+
+}
